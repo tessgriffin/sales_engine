b7ce39e956d56b4566c018a751a33d1571f1ab34
Tyler Graham <Tyler@Tylers-MacBook-Pro.local>
2015-03-04 09:38:52 -0700

    adds first find_by method

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -10,4 +10,47 @@ class CustomerRepository
   def random
     @customers.sample(1).first
   end
+
+  def find_by_id(input)
+    @customers.find do |x|
+      x.id == input
+    end
+  end
+
+  def find_by_first_name(input)
+
+  end
+
+  def find_by_last_name(input)
+
+  end
+
+  def find_by_created_at(input)
+
+  end
+
+  def find_by_updated_at(input)
+
+  end
+
+  def find_all_by_id(input)
+
+  end
+
+  def find_all_by_first_name(input)
+
+  end
+
+  def find_all_by_last_name(input)
+
+  end
+
+  def find_all_by_created_at(input)
+
+  end
+
+  def find_all_by_updated_at(input)
+
+  end
+
 end
