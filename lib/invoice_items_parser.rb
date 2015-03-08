201f385cab193355f2f8430b2c16f5a6c13de3a5
Tess Griffin <tessjgriffin@gmail.com>
2015-03-08 11:53:28 -0600

    s/InvoiceItemParser/InvoiceItemsParser/g

--- lib/invoice_items_parser.rb
+++ lib/invoice_items_parser.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-class InvoiceItemParser
+class InvoiceItemsParser
   def self.call(file_name)
     CSV.open(file_name, headers: true)
   end
--- test/invoice_items_parser_test.rb
+++ test/invoice_items_parser_test.rb
@@ -4,21 +4,21 @@ require_relative '../lib/invoice_items_parser'
 class InvoiceItemsParserTest < Minitest::Test
 
   def test_it_exists
-    assert InvoiceItemParser
+    assert InvoiceItemsParser
   end
 
   def test_it_returns_array_of_one_item
-    output = InvoiceItemParser.call("./data/fake_invoice_items.csv")
+    output = InvoiceItemsParser.call("./data/fake_invoice_items.csv")
     assert output
   end
 
   def test_it_returns_array_of_multiple_items
-    output = InvoiceItemParser.call("./data/fake_invoice_items.csv")
+    output = InvoiceItemsParser.call("./data/fake_invoice_items.csv")
     assert_equal 12, output.count
   end
 
   def test_it_returns_array_of_all_items
-    output = InvoiceItemParser.call("./data/invoice_items.csv")
+    output = InvoiceItemsParser.call("./data/invoice_items.csv")
     assert_equal 21687, output.count
   end
 
